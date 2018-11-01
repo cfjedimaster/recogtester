9b9ac54aa3e2043b3f3182e8a5fafbb3e75ef1ce
Raymond Camden <raymondcamden@gmail.com>
2018-11-01 16:13:11 -0500

    updates to ibm stuff

--- ibm.js
+++ ibm.js
@@ -1,51 +1,59 @@
-var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-var fs = require('fs');
+const VisualRecognitionV3 = require("watson-developer-cloud/visual-recognition/v3");
+const fs = require("fs");
 
 function doProcess(path, auth) {
-
 	return new Promise((resolve, reject) => {
 
-		console.log('Attempting IBM recog');
-		var visual_recognition = new VisualRecognitionV3({
-			api_key: auth.api_key,
-			version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20
+		console.log("Attempting IBM recog");
+		let visual_recognition = new VisualRecognitionV3({
+			url:'https://gateway.watsonplatform.net/visual-recognition/api',
+			iam_apikey: auth.api_key,
+			version: '2018-03-19'
 		});
-		
-		var params = {
+
+		/*
+		11/1/2018
+		For some reason, both API calls using the same stream caused some kind of
+		weird lock, sync issue. My workaround is to just use to streams, but it 
+		feels weird.		
+		*/
+		let params = {
+			images_file: fs.createReadStream(path)
+		};
+
+		let params2 = {
 			images_file: fs.createReadStream(path)
 		};
 
 		let mainP = new Promise((resolve, reject) => {
-
 			visual_recognition.classify(params, function(err, res) {
-			if (err)
-				reject(err);
-			else
-				resolve(res);
+				if (err) reject(err);
+				else resolve(res);
 			});
-
 		});
 
 		let faceP = new Promise((resolve, reject) => {
-			visual_recognition.detectFaces(params, function(err, res) {
-			if (err)
-				reject(err);
-			else
-				resolve(res);
+			visual_recognition.detectFaces(params2, function(err, res) {
+				if (err) reject(err);
+				else resolve(res);
 			});
 		});
 
-		Promise.all([mainP, faceP]).then(values => {
+		Promise.all([mainP, faceP])
+		.then(values => {
 			let result = {
-				main:values[0].images[0],
-				faces:values[1].images[0]
+				main: values[0].images[0],
+				faces: values[1].images[0]
 			};
-            resolve({"ibm":result});
+			return resolve({ ibm: result });
+		})
+		.catch(e => {
+			console.log("in IBMs catch");
+			console.error(e);
+			return reject(e);
 		});
+  });
 
-	});
+}
 
-};
-
-module.exports = { doProcess }
-
+module.exports = { doProcess };
